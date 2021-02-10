46b644797b993e50db803ac48fdf75988f23b161
Remigiusz Drobinski <30534425+curiosis@users.noreply.github.com>
2021-02-10 13:03:56 +0100

    Add flash

--- GeometryRush/Assets/Scripts/LevelManager.cs
+++ GeometryRush/Assets/Scripts/LevelManager.cs
@@ -1,23 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
-    public static bool startLevel = false;
+    public static bool flashBool = false, flash;
     public Transform player, finish;
     public static float distance;
 
+    public CanvasGroup image;
+    public Color color;
+
     void Update()
     {
+        if (flashBool)
+        {
+            image.alpha -= Time.deltaTime;
+            if(image.alpha <= 0)
+            {
+                image.alpha = 0;
+                flashBool = false;
+            }
+        }
+
+        if (flash)
+        {
+            flashBool = true;
+            image.alpha = 1;
+            flash = false;
+        }
 
         distance = (player.position.x / finish.position.x);
-
-        Debug.Log((distance*100).ToString("0")+"%");
-
-        if (Input.GetKeyUp(KeyCode.Space) && !startLevel)
-        {
-            startLevel = true;
-        }
+        //Debug.Log((distance*100).ToString("0")+"%");
+        //Debug.Log(flashBool);
     }
 }
