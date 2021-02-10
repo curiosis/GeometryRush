1b08dbc648848e479e9de141d7371257c1056dd5
Remigiusz Drobinski <30534425+curiosis@users.noreply.github.com>
2021-02-10 05:34:32 +0100

    Add progress bar

--- GeometryRush/Assets/Scripts/LevelManager.cs
+++ GeometryRush/Assets/Scripts/LevelManager.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class LevelManager : MonoBehaviour
 {
     public static bool startLevel = false;
+    public Transform player, finish;
+    public static float distance;
 
     void Update()
     {
+
+        distance = (player.position.x / finish.position.x);
+
+        Debug.Log(distance);
+
         if (Input.GetKeyUp(KeyCode.Space) && !startLevel)
         {
             startLevel = true;
