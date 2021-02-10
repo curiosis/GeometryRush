644aedb42578c7bdd1162ccf1387069a73a32b37
Remigiusz Drobinski <30534425+curiosis@users.noreply.github.com>
2021-02-10 07:27:34 +0100

    Add new type movement player

--- GeometryRush/Assets/Scripts/PlayerMovement.cs
+++ GeometryRush/Assets/Scripts/PlayerMovement.cs
@@ -17,14 +17,18 @@ public class PlayerMovement : MonoBehaviour
     public float checkRadius;
     public LayerMask whatIsGround;
 
-    private float posX, posY;
+    private float posX, posY, posYgroundCheck;
+
+    public int type;
 
 
     void Start()
     {
+        type = 0;
         posX = -1;
         posY = -1;
         rb2D = GetComponent<Rigidbody2D>();
+        ChangeRbValues();
     }
 
     
@@ -43,10 +47,47 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        if(type == 0 || type == 2)
+        {
             isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
             if (isGrounded && Input.GetKeyDown(KeyCode.Space))
             {
-                rb2D.velocity = Vector3.up * jumpForce;
+                if(type == 0)
+                    rb2D.velocity = Vector3.up * jumpForce;
+                if (type == 2)
+                    rb2D.velocity = Vector3.down * jumpForce;
             }
+        }
+
+            
+        if(type == 1)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                rb2D.velocity = Vector3.up * jumpForce / 3;
+            }
+        }
+    }
+
+    void ChangeRbValues()
+    {
+        if (type == 0)
+        {
+            rb2D.mass = 5;
+            rb2D.gravityScale = 5;
+        }
+
+        if (type == 1)
+        {
+            rb2D.mass = 3;
+            rb2D.gravityScale = 3;
+        }
+
+        if (type == 2)
+        {
+            rb2D.mass = 5;
+            rb2D.gravityScale = -5;
+            transform.Rotate(0, 0, 180);
+        }
     }
 }
