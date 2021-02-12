8d15ccd293b8f6d234affbdf23e3d8d064928673
Remigiusz Drobinski <30534425+curiosis@users.noreply.github.com>
2021-02-12 12:17:06 +0100

    Add ui progress text

--- GeometryRush/Assets/Scripts/PlayerCollision.cs
+++ GeometryRush/Assets/Scripts/PlayerCollision.cs
@@ -60,6 +60,7 @@ public class PlayerCollision : MonoBehaviour
 
         if (collision.CompareTag("ChangeType0"))
         {
+            Debug.Log(BackgroundMelody.time);
             player.type = 0;
             plane.SetActive(false);
             Background.index += 1;
@@ -68,6 +69,7 @@ public class PlayerCollision : MonoBehaviour
 
         if (collision.CompareTag("ChangeType1"))
         {
+            Debug.Log(BackgroundMelody.time);
             player.type = 1;
             plane.SetActive(true);
             Background.index += 1;
@@ -76,6 +78,7 @@ public class PlayerCollision : MonoBehaviour
 
         if (collision.CompareTag("ChangeType2"))
         {
+            BackgroundMelody.startMelody = true;
             player.type = 2;
             plane.SetActive(false);
             Background.index += 1;
--- GeometryRush/Assets/Scripts/ProgressBar.cs
+++ GeometryRush/Assets/Scripts/ProgressBar.cs
@@ -1,12 +1,38 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ProgressBar : MonoBehaviour
 {
     public Slider progressBar;
+    public Text progressValue;
+    bool anim;
+    int d;
 
     void Update()
     {
+
         progressBar.value = LevelManager.distance;
+        d = (int)(LevelManager.distance * 100);
+        progressValue.text = d.ToString("0") + "%";
+
+        if (d>1 && d % 10 == 0 && !anim)
+        {
+            anim = true;
+            progressValue.fontSize = 60;
+            StartCoroutine(Anim());
+        }
+        if ((d > 5 && d % 10 == 5)||d==1)
+            anim = false;
+    }
+
+    IEnumerator Anim()
+    {
+        yield return new WaitForSeconds(0.1f);
+        for (int i = 0; i < 30; i++)
+        {
+            progressValue.fontSize -= 1;
+            yield return new WaitForSeconds(0.13333f);
+        }
     }
 }
